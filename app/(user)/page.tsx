8acd73b7400544fbf251c635ed1d4cc371f2f172
dewedeth <yousefbacarro@icloud.com>
2023-02-03 03:16:16 -0500

    dewedeth

--- app/(user)/page.tsx
+++ app/(user)/page.tsx
@@ -1,9 +1,22 @@
-import React from 'react';
+import { previewData } from "next/headers";
+import { groq } from "next-sanity";
 
-function page() {
-  return (
-    <div></div>
-  );
+const query = groq`
+  *[_type=='post'] {
+  ...,
+  author->,
+  categories[]->
+  }| order(_createAt desc)
+`
+
+export default function HomePage() {
+
+  if (previewData()) {
+    return <div>Preview mode</div>
+  }
+
+
+  return <div>
+    <h1>Not in Preview mode</h1>
+  </div>;
 }
-
-export default page;
