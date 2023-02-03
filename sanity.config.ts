8fcd3c2001c5ce39cc2dce4cefeccbd1c9d9aa46
dewedeth <yousefbacarro@icloud.com>
2023-02-02 23:07:00 -0500

    dewedeth

--- sanity.config.ts
+++ sanity.config.ts
@@ -3,12 +3,16 @@ import {deskTool} from 'sanity/desk'
 import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemas'
 
-export default defineConfig({
-  name: 'default',
-  title: 'nextjs13 sanity blog',
+const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!;
+const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET!;
 
-  projectId: 'f97b2g2q',
-  dataset: 'production',
+export default defineConfig({
+  basePath: "/studio",
+  name: 'YOUSEF_Content_Studio',
+  title: 'Yousef Content Studio',
+
+  projectId,
+  dataset,
 
   plugins: [deskTool(), visionTool()],
 
